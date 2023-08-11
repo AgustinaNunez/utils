b3eb89c70e36a29a2a3a2d2e3458023614598e9a
Agustina Nuñez <aguusnm@gmail.com>
2023-08-11 16:32:37 -0300

    Some minor changes

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,5 +1,14 @@
 #!/bin/bash -eu
 
 jest --coverage
-# TODO: open and start are Mac commands
-open coverage/lcov-report/index.html || start coverage/lcov-report/index.html
+
+# TODO: comandos probado para macosx, validar si para windows y linux funcionan
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    xdg-open coverage/lcov-report/index.html
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    open coverage/lcov-report/index.html
+elif [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
+    start coverage/lcov-report/index.html
+else
+    echo "Sistema operativo no compatible"
+fi
--- scripts/tag.sh
+++ scripts/tag.sh
@@ -3,4 +3,4 @@
 version=$(node -p "require('./package.json').version")
 git tag -d v$version
 git tag -a v$version -m "Versión $version"
-git push -f origin v$version # TODO: eliminar el -f para no hacer un override del tag remoto
+git push origin v$version
